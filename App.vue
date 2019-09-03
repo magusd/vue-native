586d9722e1436ec6e25e5e6f9019c2a17798ccff
Lobs <magusd@users.noreply.github.com>
2019-09-03 14:47:54 -0300

    first commit

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text class="text-color-light">Trizotte viadao</text>
+  </view>
+</template>
+
+<style>
+.container {
+  color: white;
+  background-color: purple;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
